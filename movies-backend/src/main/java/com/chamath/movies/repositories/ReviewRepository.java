f368d40eef18e27d6995bc8e18f5762362eecbd7
Chamath Wijerathne <anuradhika.wije@gmail.com>
2023-06-16 17:50:09 +0300

    Completed review model, repository, service and controller.

---
+++ movies-backend/src/main/java/com/chamath/movies/repositories/ReviewRepository.java
@@ -0,0 +1,10 @@
+package com.chamath.movies.repositories;
+
+import com.chamath.movies.models.Review;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
